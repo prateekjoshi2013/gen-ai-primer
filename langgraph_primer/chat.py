62b14b5b9fe3a432497f8f597c9222e8e02aa9d4
Prateek <prateekjoshi2013@gmail.com>
2026-01-03 02:26:18 +0000

    Added llm support to chatbot

--- langgraph_primer/chat.py
+++ langgraph_primer/chat.py
@@ -4,6 +4,12 @@ from marshmallow import pprint
 from typing_extensions import TypedDict
 from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, START, END
+from langchain.chat_models import init_chat_model
+
+llm = init_chat_model(
+    model="gpt-4.1-mini",
+    model_provider="openai",
+)
 
 
 class State(TypedDict):
@@ -16,7 +22,8 @@ def chatbot(state: State) -> State:
     # Here you can implement the logic for the chatbot using the messages in the state.
     # For example, you could call an LLM to generate a response based on the chat history.
     # and on returning because of the Annotated type, new messages will be added to state['messages']
-    return {"messages": ["Hi, this is a message from Chatbot Node."]}
+    response = llm.invoke(state["messages"])
+    return {"messages": [response]}
 
 
 def samplenode(state: State) -> State:
